6e8e1c6f943cc1f1b49306a82d6375dd800ff058
Søren Englund <S_englund0110@hotmail.com>
2025-03-08 18:31:33 +0100

    Support wildcards (#17)
    * Added support for using wildcards
    
    * Refactord test cases
    
    * Updated packages

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "vitest";
+import { expect, test, describe } from "vitest";
 import {
   buildBreadcrumbTrail,
   matchBreadcrumbByPath,
@@ -6,149 +6,217 @@ import {
 } from "../src/utils";
 import { Breadcrumb } from "../src/types";
 
-// matchBreadcrumbByPath
-test("getBreadcrumbByPath finds breadcrumb for given path", () => {
-  const breadcrumbs: Breadcrumb[] = [
-    { path: "/", label: "Home" },
-    { path: "/users", label: "Users" },
-    { path: "/users/{id}", label: "User" },
-    { path: "/users/{id}/posts", label: "Posts" },
-    { path: "/users/{id}/posts/{id}", label: "Post" },
-    { path: "/about", label: "About" },
-  ];
+// Shared breadcrumbs data
+const breadcrumbs: Breadcrumb[] = [
+  { path: "/", label: "Home" },
+  { path: "/users", label: "Users" },
+  { path: "/users/{id}", label: "User" },
+  { path: "/users/{id}/posts", label: "Posts" },
+  { path: "/users/{id}/posts/{id}", label: "Post" },
+  { path: "/users/{id}/posts/{id}/*", label: "Post" },
+  { path: "/users/{id}/posts/{id}/*/comments", label: "Comments" },
+  { path: "/users/{id}/posts/{id}/*/likes/*", label: "Likes" },
+  { path: "/about", label: "About" },
+];
 
-  expect(matchBreadcrumbByPath("/", breadcrumbs)).toStrictEqual({
-    path: "/",
-    label: "Home",
-  });
-
-  expect(matchBreadcrumbByPath("/users", breadcrumbs)).toStrictEqual({
-    path: "/users",
-    label: "Users",
-  });
-
-  expect(matchBreadcrumbByPath("/users/123", breadcrumbs)).toStrictEqual({
-    path: "/users/{id}",
-    label: "User",
-  });
-
-  expect(matchBreadcrumbByPath("/users/123/posts", breadcrumbs)).toStrictEqual({
-    path: "/users/{id}/posts",
-    label: "Posts",
-  });
-
-  expect(
-    matchBreadcrumbByPath("/users/123/posts/{id}", breadcrumbs)
-  ).toStrictEqual({
+const breadcrumbsWithParents: Breadcrumb[] = [
+  { path: "/", label: "Home" },
+  { path: "/users", label: "Users", parent: "/" },
+  { path: "/users/{id}", label: "User", parent: "/users" },
+  { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
+  {
     path: "/users/{id}/posts/{id}",
     label: "Post",
+    parent: "/users/{id}/posts",
+  },
+  {
+    path: "/users/{id}/posts/{id}/*",
+    label: "Post",
+    parent: "/users/{id}/posts/{id}",
+  },
+  {
+    path: "/users/{id}/posts/{id}/*/comments",
+    label: "Comments",
+    parent: "/users/{id}/posts/{id}/*",
+  },
+  { path: "/about", label: "About" },
+];
+
+// Tests
+describe("matchBreadcrumbByPath", () => {
+  test.each([
+    ["/", { path: "/", label: "Home" }],
+    ["/users", { path: "/users", label: "Users" }],
+    ["/users/123", { path: "/users/{id}", label: "User" }],
+    ["/users/123/posts", { path: "/users/{id}/posts", label: "Posts" }],
+    [
+      "/users/123/posts/{id}",
+      { path: "/users/{id}/posts/{id}", label: "Post" },
+    ],
+    [
+      "/users/123/posts/{id}/wildcard",
+      { path: "/users/{id}/posts/{id}/*", label: "Post" },
+    ],
+    [
+      "/users/123/posts/{id}/wildcard/comments",
+      { path: "/users/{id}/posts/{id}/*/comments", label: "Comments" },
+    ],
+    [
+      "/users/123/posts/{id}/wildcard/likes/anotherwildcard",
+      { path: "/users/{id}/posts/{id}/*/likes/*", label: "Likes" },
+    ],
+  ])("finds breadcrumb for path %s", (path, expected) => {
+    expect(matchBreadcrumbByPath(path, breadcrumbs)).toStrictEqual(expected);
   });
 });
 
-// buildBreadcrumbTrail
-test("buildTrail returns breadcrumbs for given path", () => {
-  const breadcrumbs: Breadcrumb[] = [
-    { path: "/", label: "Home" },
-    { path: "/users", label: "Users", parent: "/" },
-    { path: "/users/{id}", label: "User", parent: "/users" },
-    { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
-    {
-      path: "/users/{id}/posts/{id}",
-      label: "Post",
-      parent: "/users/{id}/posts",
-    },
-    { path: "/about", label: "About" },
-  ];
-
-  expect(buildBreadcrumbTrail("/does-not-exist", breadcrumbs)).toStrictEqual(
-    []
-  );
-
-  expect(buildBreadcrumbTrail("/", breadcrumbs)).toStrictEqual([
-    { path: "/", label: "Home" },
-  ]);
-
-  expect(buildBreadcrumbTrail("/users", breadcrumbs)).toStrictEqual([
-    { path: "/", label: "Home" },
-    { path: "/users", label: "Users", parent: "/" },
-  ]);
-
-  expect(buildBreadcrumbTrail("/users/123", breadcrumbs)).toStrictEqual([
-    { path: "/", label: "Home" },
-    { path: "/users", label: "Users", parent: "/" },
-    { path: "/users/{id}", label: "User", parent: "/users" },
-  ]);
-
-  expect(buildBreadcrumbTrail("/users/123/posts", breadcrumbs)).toStrictEqual([
-    { path: "/", label: "Home" },
-    { path: "/users", label: "Users", parent: "/" },
-    { path: "/users/{id}", label: "User", parent: "/users" },
-    { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
-  ]);
-
-  expect(
-    buildBreadcrumbTrail("/users/123/posts/321", breadcrumbs)
-  ).toStrictEqual([
-    { path: "/", label: "Home" },
-    { path: "/users", label: "Users", parent: "/" },
-    { path: "/users/{id}", label: "User", parent: "/users" },
-    { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
-    {
-      path: "/users/{id}/posts/{id}",
-      label: "Post",
-      parent: "/users/{id}/posts",
-    },
-  ]);
+describe("buildBreadcrumbTrail", () => {
+  test.each([
+    ["/does-not-exist", []],
+    ["/", [{ path: "/", label: "Home" }]],
+    [
+      "/users",
+      [
+        { path: "/", label: "Home" },
+        { path: "/users", label: "Users", parent: "/" },
+      ],
+    ],
+    [
+      "/users/123",
+      [
+        { path: "/", label: "Home" },
+        { path: "/users", label: "Users", parent: "/" },
+        { path: "/users/{id}", label: "User", parent: "/users" },
+      ],
+    ],
+    [
+      "/users/123/posts",
+      [
+        { path: "/", label: "Home" },
+        { path: "/users", label: "Users", parent: "/" },
+        { path: "/users/{id}", label: "User", parent: "/users" },
+        { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
+      ],
+    ],
+    [
+      "/users/123/posts/321",
+      [
+        { path: "/", label: "Home" },
+        { path: "/users", label: "Users", parent: "/" },
+        { path: "/users/{id}", label: "User", parent: "/users" },
+        { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
+        {
+          path: "/users/{id}/posts/{id}",
+          label: "Post",
+          parent: "/users/{id}/posts",
+        },
+      ],
+    ],
+    [
+      "/users/123/posts/321/wildcard",
+      [
+        { path: "/", label: "Home" },
+        { path: "/users", label: "Users", parent: "/" },
+        { path: "/users/{id}", label: "User", parent: "/users" },
+        { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
+        {
+          path: "/users/{id}/posts/{id}",
+          label: "Post",
+          parent: "/users/{id}/posts",
+        },
+        {
+          path: "/users/{id}/posts/{id}/*",
+          label: "Post",
+          parent: "/users/{id}/posts/{id}",
+        },
+      ],
+    ],
+    [
+      "/users/123/posts/321/wildcard/comments",
+      [
+        { path: "/", label: "Home" },
+        { path: "/users", label: "Users", parent: "/" },
+        { path: "/users/{id}", label: "User", parent: "/users" },
+        { path: "/users/{id}/posts", label: "Posts", parent: "/users/{id}" },
+        {
+          path: "/users/{id}/posts/{id}",
+          label: "Post",
+          parent: "/users/{id}/posts",
+        },
+        {
+          path: "/users/{id}/posts/{id}/*",
+          label: "Post",
+          parent: "/users/{id}/posts/{id}",
+        },
+        {
+          path: "/users/{id}/posts/{id}/*/comments",
+          label: "Comments",
+          parent: "/users/{id}/posts/{id}/*",
+        },
+      ],
+    ],
+  ])("returns breadcrumb trail for path %s", (path, expected) => {
+    expect(buildBreadcrumbTrail(path, breadcrumbsWithParents)).toStrictEqual(
+      expected
+    );
+  });
 });
 
-// transformPath
-test("should replace placeholders with corresponding values", () => {
-  let breadcrumb: Breadcrumb | undefined = undefined;
-  breadcrumb = { path: "/users", label: "Users", parent: "/" };
-  expect(replacePathParams(breadcrumb.path, breadcrumb.params)).toStrictEqual(
-    "/users"
-  );
-
-  breadcrumb = {
-    path: "/users/{id}",
-    label: "User",
-    params: [{ key: "ids", value: "123" }], // Incorrect key
-    parent: "/users",
-  };
-  expect(replacePathParams(breadcrumb.path, breadcrumb.params)).toStrictEqual(
-    "/users/{id}"
-  );
-
-  breadcrumb = {
-    path: "/users/{id}",
-    label: "User",
-    params: [{ key: "id", value: "123" }],
-    parent: "/users",
-  };
-  expect(replacePathParams(breadcrumb.path, breadcrumb.params)).toStrictEqual(
-    "/users/123"
-  );
-
-  breadcrumb = {
-    path: "/users/{id}/posts",
-    label: "Posts",
-    params: [{ key: "id", value: "123" }],
-    parent: "/users",
-  };
-  expect(replacePathParams(breadcrumb.path, breadcrumb.params)).toStrictEqual(
-    "/users/123/posts"
-  );
-
-  breadcrumb = {
-    path: "/users/{id}/posts/{id}",
-    label: "Posts",
-    params: [
-      { key: "id", value: "123" },
-      { key: "id", value: "312" },
+describe("replacePathParams", () => {
+  test.each([
+    [{ path: "/users", label: "Users", parent: "/" }, [], "/users"],
+    [
+      {
+        path: "/users/{id}",
+        label: "User",
+        parent: "/users",
+        params: [{ key: "ids", value: "123" }],
+      },
+      [{ key: "ids", value: "123" }],
+      "/users/{id}",
     ],
-    parent: "/users",
-  };
-  expect(replacePathParams(breadcrumb.path, breadcrumb.params)).toStrictEqual(
-    "/users/123/posts/312"
+    [
+      {
+        path: "/users/{id}",
+        label: "User",
+        parent: "/users",
+        params: [{ key: "id", value: "123" }],
+      },
+      [{ key: "id", value: "123" }],
+      "/users/123",
+    ],
+    [
+      {
+        path: "/users/{id}/posts",
+        label: "Posts",
+        parent: "/users",
+        params: [{ key: "id", value: "123" }],
+      },
+      [{ key: "id", value: "123" }],
+      "/users/123/posts",
+    ],
+    [
+      {
+        path: "/users/{id}/posts/{id}",
+        label: "Posts",
+        parent: "/users",
+        params: [
+          { key: "id", value: "123" },
+          { key: "id", value: "312" },
+        ],
+      },
+      [
+        { key: "id", value: "123" },
+        { key: "id", value: "312" },
+      ],
+      "/users/123/posts/312",
+    ],
+  ])(
+    "replaces placeholders in path %s with params %s",
+    (breadcrumb, params, expected) => {
+      expect(replacePathParams(breadcrumb.path, params)).toStrictEqual(
+        expected
+      );
+    }
   );
 });
