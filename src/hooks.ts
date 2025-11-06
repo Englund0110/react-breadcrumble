e70004d7ee68304a365a818b9825c19c3489e7f9
Søren Englund <S_englund0110@hotmail.com>
2025-11-06 22:35:34 +0100

    Move to tsdown instead of rollup + refactor and new helper methods (#24)
    * Update LICENSE file
    
    * Remove extra line
    
    * Support wildcards (#17)
    
    * Added support for using wildcards
    
    * Refactord test cases
    
    * Updated packages
    
    * 1.4.0-alpha1
    
    * Updated readme
    
    * 1.4.0
    
    * Update yml for pnpm
    
    * Run pnpm install
    
    * Remove if present
    
    * Fix yaml
    
    * Minor changes
    
    * Update nodejs yml
    
    * moved from rollup to tsdown
    
    * Added type to package json
    
    * 2.0.0-alpha1
    
    * chore: release v2.0.0-alpha1.0
    
    * fix: update Breadcrumb type to allow undefined for params and parent
    
    * chore: release v2.0.0-alpha1.1
    
    * Merged changes into branch
    
    * chore: release v2.0.0-alpha1.2
    
    * Fix spelling
    
    * chore: release v2.0.0-alpha1.3
    
    * Added documentation/comments
    
    * chore: release v2.0.0-alpha1.4
    
    * chore: release v2.0.0-alpha1.5
    
    * minor renaming
    
    * chore: release v2.0.0-alpha1.6
    
    * update readme

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,23 +1,14 @@
-import { useContext, useMemo } from "react";
+import { useContext } from "react";
 import { BreadcrumbContext } from "./BreadcrumbContext";
-import { buildBreadcrumbTrail } from "./utils";
-import { Breadcrumb } from "./types";
 
+/**
+ * Hook to access breadcrumb context methods and state.
+ * @returns
+ */
 export const useBreadcrumbs = () => {
   const context = useContext(BreadcrumbContext);
   if (!context) {
     throw new Error("useBreadcrumbs must be used within a BreadcrumbProvider");
   }
   return context;
-};
-
-/**
- * @deprecated This function is deprecated. Use the `getBreadcrumbTrail` function returned by the `useBreadcrumbs` hook instead.
- */
-export const useBreadcrumbTrail = (currentPath: string): Breadcrumb[] => {
-  const { breadcrumbs } = useBreadcrumbs();
-
-  return useMemo(() => {
-    return buildBreadcrumbTrail(currentPath, breadcrumbs);
-  }, [breadcrumbs, currentPath]);
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export * from "./types";
+export {
+  type Breadcrumb,
+  type BreadcrumbContextType,
+  type BreadcrumbProviderProps,
+} from "./types";
 export { BreadcrumbProvider, BreadcrumbContext } from "./BreadcrumbContext";
-export { useBreadcrumbs, useBreadcrumbTrail } from "./hooks";
+export { useBreadcrumbs } from "./hooks";
 export {
   matchBreadcrumbByPath,
   buildBreadcrumbTrail,
