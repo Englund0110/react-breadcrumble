0e25f7e293e07f3c6e4a88043b896866368890b3
Søren Englund <S_englund0110@hotmail.com>
2025-11-10 23:36:29 +0100

    Added es-lint

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,17 @@
 /**
  * Type representing a single breadcrumb.
  */
-export type Breadcrumb = {
+export interface Breadcrumb {
   label: string;
   path: string;
   params?: { key: string; value: string }[] | undefined;
   parent?: string | undefined;
-};
+}
 
 /**
  * Type for the Breadcrumb Context.
  */
-export type BreadcrumbContextType = {
+export interface BreadcrumbContextType {
   breadcrumbs: Breadcrumb[];
   setBreadcrumbs: React.Dispatch<React.SetStateAction<Breadcrumb[]>>;
   updateBreadcrumb: (
@@ -21,12 +21,12 @@ export type BreadcrumbContextType = {
   ) => void;
   resetBreadcrumb: (path: string) => void;
   getBreadcrumbTrail: (currentPath: string) => Breadcrumb[];
-};
+}
 
 /**
  * Props for the BreadcrumbProvider component.
  */
-export type BreadcrumbProviderProps = {
+export interface BreadcrumbProviderProps {
   breadcrumbs?: Breadcrumb[];
   children: React.ReactNode;
-};
+}
